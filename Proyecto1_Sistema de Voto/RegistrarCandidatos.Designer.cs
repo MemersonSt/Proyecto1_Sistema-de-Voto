f3b5d95808f2c9f74132a18af62337078354aa3d
steve <steve@DESKTOP-T6Q5TVR>
2023-09-28 15:54:12 -0500

    Arreglando nombres de variables

--- Proyecto1_Sistema de Voto/RegistrarCandidatos.Designer.cs
+++ Proyecto1_Sistema de Voto/RegistrarCandidatos.Designer.cs
@@ -24,21 +24,21 @@
         /// </summary>
         private void InitializeComponent () {
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.btnNotificar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
             this.btnSalir = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textCedula = new System.Windows.Forms.TextBox();
+            this.textLista = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pctFoto = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctFoto)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -51,15 +51,15 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Registrar Candidatos";
             // 
-            // button1
+            // btnBuscar
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(175)))), ((int)(((byte)(211)))), ((int)(((byte)(226)))));
-            this.button1.Location = new System.Drawing.Point(296, 144);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(146, 29);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Buscar";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(175)))), ((int)(((byte)(211)))), ((int)(((byte)(226)))));
+            this.btnBuscar.Location = new System.Drawing.Point(296, 144);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(146, 29);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
             // 
             // textBox1
             // 
@@ -69,53 +69,53 @@
             this.textBox1.Size = new System.Drawing.Size(188, 29);
             this.textBox1.TabIndex = 2;
             // 
-            // textBox2
+            // textNombre
             // 
-            this.textBox2.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(276, 272);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(188, 31);
-            this.textBox2.TabIndex = 3;
+            this.textNombre.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNombre.Location = new System.Drawing.Point(276, 272);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(188, 31);
+            this.textNombre.TabIndex = 3;
             // 
-            // button2
+            // btnNotificar
             // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(127)))), ((int)(((byte)(206)))));
-            this.button2.Location = new System.Drawing.Point(793, 119);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(226, 78);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Notificar registro";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btnNotificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(127)))), ((int)(((byte)(206)))));
+            this.btnNotificar.Location = new System.Drawing.Point(793, 119);
+            this.btnNotificar.Name = "btnNotificar";
+            this.btnNotificar.Size = new System.Drawing.Size(226, 78);
+            this.btnNotificar.TabIndex = 7;
+            this.btnNotificar.Text = "Notificar registro";
+            this.btnNotificar.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // btnGuardar
             // 
-            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(108)))), ((int)(((byte)(148)))));
-            this.button3.Location = new System.Drawing.Point(835, 277);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(216, 39);
-            this.button3.TabIndex = 8;
-            this.button3.Text = "Guardar";
-            this.button3.UseVisualStyleBackColor = false;
+            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(108)))), ((int)(((byte)(148)))));
+            this.btnGuardar.Location = new System.Drawing.Point(835, 277);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(216, 39);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
             // 
-            // button4
+            // btnActualizar
             // 
-            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(108)))), ((int)(((byte)(148)))));
-            this.button4.Location = new System.Drawing.Point(835, 338);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(216, 39);
-            this.button4.TabIndex = 9;
-            this.button4.Text = "Actualizar";
-            this.button4.UseVisualStyleBackColor = false;
+            this.btnActualizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(108)))), ((int)(((byte)(148)))));
+            this.btnActualizar.Location = new System.Drawing.Point(835, 338);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(216, 39);
+            this.btnActualizar.TabIndex = 9;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = false;
             // 
-            // button5
+            // btnEliminar
             // 
-            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(108)))), ((int)(((byte)(148)))));
-            this.button5.Location = new System.Drawing.Point(835, 396);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(216, 39);
-            this.button5.TabIndex = 10;
-            this.button5.Text = "Eliminar";
-            this.button5.UseVisualStyleBackColor = false;
+            this.btnEliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(108)))), ((int)(((byte)(148)))));
+            this.btnEliminar.Location = new System.Drawing.Point(835, 396);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(216, 39);
+            this.btnEliminar.TabIndex = 10;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
             // 
             // btnSalir
             // 
@@ -138,21 +138,21 @@
             this.label2.TabIndex = 12;
             this.label2.Text = "Nombre:";
             // 
-            // textBox3
+            // textCedula
             // 
-            this.textBox3.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(276, 320);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(188, 31);
-            this.textBox3.TabIndex = 13;
+            this.textCedula.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textCedula.Location = new System.Drawing.Point(276, 320);
+            this.textCedula.Name = "textCedula";
+            this.textCedula.Size = new System.Drawing.Size(188, 31);
+            this.textCedula.TabIndex = 13;
             // 
-            // textBox4
+            // textLista
             // 
-            this.textBox4.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(276, 372);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(188, 31);
-            this.textBox4.TabIndex = 14;
+            this.textLista.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLista.Location = new System.Drawing.Point(276, 372);
+            this.textLista.Name = "textLista";
+            this.textLista.Size = new System.Drawing.Size(188, 31);
+            this.textLista.TabIndex = 14;
             // 
             // label3
             // 
@@ -168,19 +168,19 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(74, 375);
+            this.label4.Location = new System.Drawing.Point(202, 375);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(180, 23);
+            this.label4.Size = new System.Drawing.Size(52, 23);
             this.label4.TabIndex = 16;
-            this.label4.Text = "Fecha de nacimiento:";
+            this.label4.Text = "Lista:";
             // 
-            // pictureBox1
+            // pctFoto
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(522, 241);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(192, 194);
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
+            this.pctFoto.Location = new System.Drawing.Point(522, 241);
+            this.pctFoto.Name = "pctFoto";
+            this.pctFoto.Size = new System.Drawing.Size(192, 194);
+            this.pctFoto.TabIndex = 6;
+            this.pctFoto.TabStop = false;
             // 
             // RegistrarCandidatos
             // 
@@ -190,22 +190,22 @@
             this.ClientSize = new System.Drawing.Size(1084, 661);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textLista);
+            this.Controls.Add(this.textCedula);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnSalir);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnNotificar);
+            this.Controls.Add(this.pctFoto);
+            this.Controls.Add(this.textNombre);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.label1);
             this.Name = "RegistrarCandidatos";
             this.Text = "RegistrarCandidatos";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctFoto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -214,18 +214,18 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.PictureBox pctFoto;
+        private System.Windows.Forms.Button btnNotificar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
         private System.Windows.Forms.Button btnSalir;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textCedula;
+        private System.Windows.Forms.TextBox textLista;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
     }
