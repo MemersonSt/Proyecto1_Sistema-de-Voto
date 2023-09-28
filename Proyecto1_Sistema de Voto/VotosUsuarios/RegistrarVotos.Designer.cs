72e6c2a32bc3b476b11fb1a30b977235723a1663
steve <steve@DESKTOP-T6Q5TVR>
2023-09-28 18:26:28 -0500

    Emer

---
+++ Proyecto1_Sistema de Voto/VotosUsuarios/RegistrarVotos.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Proyecto1_Sistema_de_Voto.VotosUsuarios {
+    partial class RegistrarVotos {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent () {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(96, 107);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(905, 579);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(351, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 43);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Papeleta de Votación";
+            // 
+            // RegistrarVotos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 811);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "RegistrarVotos";
+            this.Text = "RegistrarVotos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
