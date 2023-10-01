581851c0779157ffbd76f0c45a1b1f9bc207ecd0
steve <steve@DESKTOP-T6Q5TVR>
2023-10-01 13:15:59 -0500

    creo que habra conflicto xd

---
+++ Proyecto1_Sistema de Voto/Models/Votos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1_Sistema_de_Voto.Models {
+    public class Votos {
+        public string nombreCandidato { get; set; }
+        public string archivoVotos { get; set; }
+        public string _sCedula { get; set; }
+        public string _sProvincia { get; set; }
+
+
+        public Votos () { }
+        public Votos(string nombreCandidato, string _sCedula, string _sProvincia) {
+            this.nombreCandidato = nombreCandidato;
+            this._sCedula = _sCedula;
+            this._sProvincia = _sProvincia;
+        }
+    }
+}
